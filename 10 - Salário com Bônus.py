4b271220b8178b36e30e9b0d7b68d2b5cf1a791e
Kayan Brandão <kayanbrandao.krb@gmail.com>
2021-08-06 11:39:52 -0400

    update

---
+++ 10 - Salário com Bônus.py
@@ -0,0 +1,5 @@
+NAME = input()
+SALARY = float(input())
+TOTAL_SALES = float(input())
+TOTAL = SALARY + (TOTAL_SALES * 0.15)
+print(f'TOTAL = R$ {TOTAL:.2f}')
---
+++ 9 - Salário.py
@@ -0,0 +1,5 @@
+NUMBER = int(input())
+HOURS_WORKED = int(input())
+HOURS_VALUE = float(input())
+SALARY = HOURS_WORKED * HOURS_VALUE
+print(f'NUMBER = {NUMBER}\nSALARY = U$ {SALARY:.2f}')
