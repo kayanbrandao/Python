7e45e0013fd20697b0051be34c0039574b268631
Kayan Brandão <kayanbrandao.krb@gmail.com>
2021-08-20 22:13:29 -0400

    update

--- 19 - Cédulas.py
+++ 19 - Cédulas.py
@@ -1,7 +1,7 @@
 valor = int(input())
-notas = [100, 50, 20, 10, 5, 2, 1]
+n = [100, 50, 20, 10, 5, 2, 1]
 print(f'{valor}')
-for nota in notas:
-    qt_notas = int(valor / nota)
-    print(f'{qt_notas} nota(s) de R$ {nota},00')
-    valor -= qt_notas * nota
+for n in n:
+    qt_notas = int(valor / n)
+    print(f'{qt_notas} nota(s) de R$ {n},00')
+    valor -= qt_notas * n
